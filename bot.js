45f3d6230954af517d3f551fd1f49e013526e998
Tyriny <32651416+Tyriny@users.noreply.github.com>
2017-10-09 20:40:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
 
+
+client.on('ready', () => {
+  client.user.setPresence({game: {name: "(replaceme)", type: 0}});
+  console.log('I am ready!');
+});
+
+
+setInterval(() => {
+ http.get('http://discordjs-heroku.herokuapp.com');
+}, 900000);
+
+const port = process.env.PORT || 5000;
+
+client.login(process.env.BOT_TOKEN);
