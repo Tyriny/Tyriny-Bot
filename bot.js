ca4e545a41164f23f7e0d7dfa517444d4ea73795
Zandercross12 <tuoi.nguyen69@icloud.com>
2017-10-10 08:11:23 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,47 @@
-client.user.setGame('game here !')client.user.setGame('Type ~help for help')
+// Discord code requires this command!
 const Discord = require('discord.js');
+// Sets bot thing as client
 const client = new Discord.Client();
+// The config.json is what tells the bot.js what the prefix is... which is '~'.
 const config = require("./config.json");
 
-// This is how you set the game
+// This is how you set the game for the bot.
 client.on('ready', () => {
   client.user.setPresence({game: {name: "~help for help", type: 0}});
   console.log('I am ready!');
   
+  // Sets stuff for the commands
 client.on("message", async message => {
+  // Makes it so the bot doesn't glitch as easily.
   if(message.author.bot) return;
+  // Looks for the prefix.
   if(message.content.indexOf(config.prefix) !== 0) return;
   
+  // Adds args variable.
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  // adds command variable.
   const command = args.shift().toLowerCase();
 // PUT COMMANDS UNDER THIS
 // FORMAT:
 // if(command === "<command-name>") {
 // command code   
 // }
-
+  
 // This doesn't work anymore --> client.user.setGame('Type ~help for help')
 
  if(command === "tacos") {
    message.channel.sendMessage('TACOS ARE THE BEST');
  }
 
-  // Don't do "~ping", just do "ping". 
-  if(command === "~ping") {
+  // Don't do "~ping", just do "ping". The "command" already adds the '~' in front of the text.
+  if(command === "ping") {
     message.channel.sendMessage("Pong!");
 }
   
 });
   // DO NOT PUT ANY OTHER COMMANDS UNDER THIS, THE COMMANDS WON'T WORK UNDER THIS MESSAGE
 
-
+// Don't worry about this stuff yet under this
 setInterval(() => {
  http.get('http://discordjs-heroku.herokuapp.com');
 }, 900000);
