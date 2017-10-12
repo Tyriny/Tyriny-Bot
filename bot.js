9f227cc58d8787b2dffa3ccc9d4826c54c61894e
Zandercross12 <tuoi.nguyen69@icloud.com>
2017-10-12 13:21:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,11 +63,7 @@ if(command === "kys") {
 if(command === "suicide") {
      message.channel.sendMessage('If you are having suicidal thoughts, please consider :telephone: the suicide hotline! The number can be found here: http://suicidehotlines.com/international.html')
 }
-	
-if(command === "mention") {
-	message.channel.sendMessage('HI', + mention);
-}
-	
+		
 });
 
 setInterval(() => {
