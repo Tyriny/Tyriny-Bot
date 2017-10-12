6aad669021aa042225560d2dbff05b975a6d36e6
Tyriny <32651416+Tyriny@users.noreply.github.com>
2017-10-12 18:20:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,8 +55,9 @@ if(command === "8ball") {
     else message.channel.sendMessage("Usage: ~8ball [question]");
   }
 if(command === "kys") {
-     message.channel.sendMessage('I am a :robot: and thus am unable to commit suicide :x: :gun:
+     message.channel.sendMessage('I am a :robot: and thus am unable to commit suicide :x: :gun:')
 }
+
 if(command === "suicide") {
      message.channel.sendMessage('If you are having suicidal thoughts, please consider :telephone: the suicide hotline! The number can be found here: http://suicidehotlines.com/international.html
 }
