f4ae8a020cf0d87862b892a22976865efb0f58c7
Zandercross12 <tuoi.nguyen69@icloud.com>
2017-10-13 17:13:10 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,7 +66,7 @@ if(command === "suicide") {
   
 if(command === "botinfo") {
     var embed = new Discord.RichEmbed()
-      .setThumbnail(message.author.avatarURL)
+      .setThumbnail(message.user.avatarURL)
       .addField("Creators:", "This bot was made with love \nby @Tryiny_ with help from @Zandercross12!", true)
       .addField("What this bot does", "Stuff.")
       .addField("Work in Progress", ":D")
@@ -75,17 +75,30 @@ if(command === "botinfo") {
   } 
 
 // END ALL OF THE message.reply() WITH ';'
+
 if(command === "serverinfo") {
     
     var msgArray = [];
-    msgArray.push("You are currently in " + msg.channel + " (id: " + msg.channel.id + ")");
-    msgArray.push("on server **" + msg.channel.server.name + "** (id: " + msg.channel.server.id + ") (region " + msg channel.server.region + ")");
-    msgArray.push("owned by " + msg.channel.server.owner + " (id: " + msg.channel.server.owner.id + ")");
+    message.channel.sendMessage("You are currently in " + msg.channel + " (id: " + msg.channel.id + ")");
+    message.channel.sendMessage("on server **" + msg.channel.server.name + "** (id: " + msg.channel.server.id + ") (region " + msg channel.server.region + ")");
+    message.channel.sendMessage("owned by " + msg.channel.server.owner + " (id: " + msg.channel.server.owner.id + ")");
     if (msg.channel.topic) {
-      msgArray.push("The current topic is: " + msg.channel.topic);
-    }
+      message.channel.sendMessage("The current topic is: " + msg.channel.topic);
+   }
     message.reply(msgArray);
 }
+
+//if(command === "serverinfo") {
+//    
+//    var msgArray = [];
+//    msgArray.push("You are currently in " + msg.channel + " (id: " + msg.channel.id + ")");
+//    msgArray.push("on server **" + msg.channel.server.name + "** (id: " + msg.channel.server.id + ") (region " + msg channel.server.region + ")");
+//    msgArray.push("owned by " + msg.channel.server.owner + " (id: " + msg.channel.server.owner.id + ")");
+//    if (msg.channel.topic) {
+//      msgArray.push("The current topic is: " + msg.channel.topic);
+//   }
+//    message.reply(msgArray);
+//}
 
     
 if(command === "userid") {
